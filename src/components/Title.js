626f1e6d0397d57a5f728df5ab96ba243dca76b6
BekturMuratov <Muratov225@gmail.com>
2020-09-01 02:43:52 +0600

    start new project

---
+++ src/components/Title.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+function Title(){
+	return (
+		<div className="title">
+		<h1>Photo Gallery </h1>
+		<h2>Your Pictures</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+		</div>
+		)
+}
+
+export default Title;
